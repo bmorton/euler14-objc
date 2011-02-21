ff6e200ef5f560240a69619dea517928ca3629a8
Brian Morton <bmorton@sdreader.com>
2011-02-21 10:49:55 -0800

    Initial Commit

---
+++ euler14/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  euler14
+//
+//  Created by Brian Morton on 2/21/11.
+//  Copyright 2011 The San Diego Reader. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
