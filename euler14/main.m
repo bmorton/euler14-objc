e23242925f26cc5874ac611e5ad8b64afb7e28fc
Brian Morton <bmorton@sdreader.com>
2011-02-21 12:15:33 -0800

    Add code to make this project work.

--- euler14/main.m
+++ euler14/main.m
@@ -3,18 +3,98 @@
 //  euler14
 //
 //  Created by Brian Morton on 2/21/11.
-//  Copyright 2011 The San Diego Reader. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-int main (int argc, const char * argv[])
-{
+@interface Collatz : NSObject {
+    long startingTerm;
+    int numberOfTerms;
+}
+-(Collatz*)initWithStartingTerm:(long)number;
+-(int)numberOfTerms;
+-(void)setStartingTerm:(long)number;
+-(long)startingTerm;
+-(void)generateTerms;
 
+//@property (retain) NSArray *terms;
+@property (assign) int numberOfTerms;
+
+@end
+
+@implementation Collatz
+
+@synthesize numberOfTerms;
+
+-(Collatz*)initWithStartingTerm:(long)number {
+    self = [super init];
+    
+    if (self) {
+        numberOfTerms = 0;
+        [self setStartingTerm:number];
+    }
+    
+    return self;
+}
+
+-(long)startingTerm {
+    return startingTerm;
+}
+
+-(void)setStartingTerm:(long)number {
+    if (number != startingTerm) {
+        startingTerm = number;
+        [self generateTerms];
+    }
+}
+
+-(void)generateTerms {
+    if (startingTerm > 0) {
+        long currentNumber = startingTerm;
+        numberOfTerms = 1;
+        
+        while (currentNumber != 1) {
+            if (currentNumber % 2 == 0) {
+                currentNumber = (currentNumber/2);
+            } else {
+                currentNumber = ((3*currentNumber)+1);
+            }
+            numberOfTerms++;
+        }
+    } else {
+        numberOfTerms = 0;
+    }
+}
+
+-(void)dealloc {
+    [super dealloc];
+}
+
+@end
+
+int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    int highestTermCount = 0;
+    long startingTermForHighest = 0;
 
-    // insert code here...
-    NSLog(@"Hello, World!");
+    Collatz *testForLength = [[Collatz alloc] initWithStartingTerm:0];
+    
+    for (long i = 1; i <= 1000000; i++) {
+
+        testForLength.startingTerm = i;
+        
+        if ([testForLength numberOfTerms] > highestTermCount) {
+            highestTermCount = [testForLength numberOfTerms];
+            startingTermForHighest = i;
+        }
+        
+        printf("%i: %i\n", i, [testForLength numberOfTerms]);
+    }
+    
+    [testForLength release];
+    
+    printf("Highest term and count was %i with %i terms.", startingTermForHighest, highestTermCount);
 
     [pool drain];
     return 0;
